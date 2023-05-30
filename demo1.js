afd6cecab0f9172ff134301ca1af1202d82c7dc8
Sivareddy0930 <sivareddy0930@gmail.com>
2023-05-30 12:17:08 +0530

    c

---
+++ demo1.js
@@ -0,0 +1,13 @@
+// let arr=[]
+// console.log(typeof(arr));
+//--------------------------------------------------------
+let arr=[1,2,3,4,5,6]
+console.log(arr);
+//using index we can access  items in javascript
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
