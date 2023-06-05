7737907fc12184633ef2f51267692ba84171b820
Sivareddy0930 <sivareddy0930@gmail.com>
2023-06-05 18:56:24 +0530

    commit

--- classAssignments/arraymethods/fill.js
+++ classAssignments/arraymethods/fill.js
@@ -1,5 +1,10 @@
-//fill method works as a replace elements
+//The fill() method fills specified elements in an array with a  single value.
+//The fill() method overwrites the original array.
+//array.fill(value, start, end)
+
+//if we not mention start and end index .it will overwrites with all elements in array.
+
 let arr=["java","python","html","css","bootstrap"]
 console.log(arr);
-let result=arr.fill("Programing",113);
+let result=arr.fill("Programing",0,2);
 console.log(result);
