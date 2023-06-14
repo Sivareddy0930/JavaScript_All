a5fe9d57fdc98ed6e9cc0e1cb993563ab1a39c9a
Sivareddy0930 <sivareddy0930@gmail.com>
2023-06-14 18:00:54 +0530

    game

--- Assignments/ConditionalStatements/9.js
+++ Assignments/ConditionalStatements/9.js
@@ -1,17 +1,17 @@
 // write a program to find greatest of three
-// let a=1000,b=200,c=3000;
-// if(a>b && a>c){
-//     console.log("greatest a:",a);
-// }
-// else if(b>a&&b>c){
-//     console.log("greatest b:",b);
-// }
-// else{
-//     console.log("greatest c:",c);
-// }
+let a=1,b=2,c=3;
+if(a>b && a>c){
+    console.log("greatest a:",a);
+}
+else if(b>a && b>c){
+    console.log("greatest b:",b);
+}
+else{
+    console.log("greatest c:",c);
+}
 //------------------------------------------------------------------- second method
-let a=1000,b=200,c=30;
-let temp,result;
-temp=a>b?a:b;
-result=temp>c?temp:c;
-console.log(result);
+// let a=1000,b=200,c=30;
+// let temp,result;
+// temp=a>b?a:b;
+// result=temp>c?temp:c;
+// console.log(result);
