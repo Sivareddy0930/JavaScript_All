8b0680f5e271fc3cae103bae9d05b9f64d55326d
Sivareddy0930 <sivareddy0930@gmail.com>
2023-06-17 13:36:37 +0530

    demo

--- demo1.js
+++ demo1.js
@@ -1,3 +1,23 @@
+// let imageFile =Event.target.files[0];
+// let reader = new FileReader();
+// reader.readAsDataURL(imageFile);
+// reader.addEventListener('load', () => {
+//     if (reader.result) {
+//         // Convert data URL to base64 string
+//         const imageData = reader.result.split(',')[1];
+
+//         // Modify the code as needed
+//         // For example, if you want to add a prefix to the base64 string
+//         const modifiedImageData = 'prefix_' + imageData;
+
+//         setProduct({
+//             ...product,
+//             image: modifiedImageData
+//         });
+//     } else {
+//         alert('Error occurred');
+//     }
+// });
 let imageFile =Event.target.files[0];
 let reader = new FileReader();
 reader.readAsDataURL(imageFile);
