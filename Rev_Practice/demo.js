ba28e63a26d1a7f74107cc435c5498d76c0414c5
Sivareddy0930 <sivareddy0930@gmail.com>
2024-05-09 19:21:29 +0530

    js practice

---
+++ Rev_Practice/demo.js
@@ -0,0 +1,10 @@
+
+function sayHi() {
+  let x=1000;
+    console.log(phrase); //Error
+    
+      let phrase = "Hello";
+    }
+    debugger;
+    var a=100;
+    sayHi();
---
+++ Rev_Practice/demo1.js
@@ -0,0 +1,6 @@
+
+function sayHi() {
+  let x=10;
+  console.log(typeof x)
+}
+    sayHi(1,2,3,4,"ok");
